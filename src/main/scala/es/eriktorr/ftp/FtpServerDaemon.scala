799530dfb8b6023655a0cf4ec1372436224a66c6
Erik Torres <erik.torres@lifullconnect.com>
2020-04-24 16:20:22 +0200

    :recycle: Better startup information

--- src/main/scala/es/eriktorr/ftp/FtpServerDaemon.scala
+++ src/main/scala/es/eriktorr/ftp/FtpServerDaemon.scala
@@ -7,18 +7,31 @@ import org.apache.commons.daemon.{Daemon, DaemonContext}
 
 class FtpServerDaemon extends Daemon with OptionParser {
   private[this] val ftpServerRef = new AtomicReference[Option[FtpServer]](None)
-  private[this] val options = new AtomicReference[OptionMap](Map())
+  private[this] val optionsRef = new AtomicReference[OptionMap](Map())
 
   override def init(daemonContext: DaemonContext): Unit =
-    options.set(optionsFrom(daemonContext.getArguments))
+    optionsRef.set(optionsFrom(daemonContext.getArguments))
 
   override def start(): Unit = {
-    val applicationContext = ApplicationContextLoader.defaultApplicationContext
+    val options = optionsRef.get()
+
+    val applicationContext = options.get(Symbol("config")) match {
+      case Some(resourceBasename) =>
+        ApplicationContextLoader.loadApplicationContextFrom(resourceBasename)
+      case None => ApplicationContextLoader.defaultApplicationContext
+    }
 
     val ftpServer = FtpServer(applicationContext)
     ftpServer.start()
 
     ftpServerRef.set(Some(FtpServer(applicationContext)))
+
+    val ftpServerConfig = applicationContext.ftpServerConfig
+    val hdfsClientConfig = applicationContext.hdfsClientConfig
+    logger.info(
+      s"FTP server listening in ${ftpServerConfig.hostname
+        .getOrElse("localhost")}/${ftpServerConfig.port} and connected to ${hdfsClientConfig.uri}"
+    )
   }
 
   override def stop(): Unit = ftpServerRef.get() match {
